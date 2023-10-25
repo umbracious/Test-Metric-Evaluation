b34e5756d5daebe49ca4a412ed2ae919f3b25b63
umbracious <63139410+umbracious@users.noreply.github.com>
2023-10-25 01:03:08 -0400

    avoid divide by zero

--- src/main/java/dc.java
+++ src/main/java/dc.java
@@ -5,6 +5,11 @@ public class dc {
         int linesOfCode = cloc.calculateCLOC(fileName);
         int commentLines = linesOfCode - tloc.calculateTLOC(fileName);
 
+        if (linesOfCode == 0){
+            System.err.println("Erreur: aucun ligne de code");
+            System.exit(1);
+        }
+
         float commentDensity = (float) commentLines/(float) linesOfCode;
         return commentDensity;
     }
--- src/main/java/tcmp.java
+++ src/main/java/tcmp.java
@@ -3,6 +3,10 @@ public class tcmp {
         int TLOC = tloc.calculateTLOC(fileName);
         int TASSERT = tassert.calculateTASSERT(fileName);
 
+        if (TASSERT == 0){
+            System.err.println("Erreur: pas de asserts dans le code");
+            System.exit(1);
+        }
         float tcmp = (float) TLOC/(float) TASSERT;
         return tcmp;
     }
